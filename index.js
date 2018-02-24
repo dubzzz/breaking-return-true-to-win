448d6dbb2ad6534afe86643932a6bf7cc757b5c1
Nicolas DUBIEN <github@dubien.org>
2018-02-24 01:38:53 +0100

    Use wrapIt helper

--- index.js
+++ index.js
@@ -3,35 +3,44 @@
 const fc = require('fast-check');
 const settings = {num_runs: 100000};
 
+const wrapIt = function(fn) {
+    return (...args) => {
+        try { return !fn(...args); }
+        catch (err) { return true; }
+    };
+};
+
 describe('Breaking \'return true to win\' using Property based Testing', () => {
+    // Those examples have been extracted from
+    // the website: https://alf.nu/ReturnTrue
     it('id', () => {
         function id(x) {
             return x;
         }
-        fc.assert(fc.property(fc.anything(), x => !id(x)), settings);
+        fc.assert(fc.property(fc.anything(), x => wrapIt(id)(x)), settings);
     });
     it('reflexive', () => {
         function reflexive(x) {
             return x != x;
         }
-        fc.assert(fc.property(fc.anything(), x => !reflexive(x)), settings);
+        fc.assert(fc.property(fc.anything(), x => wrapIt(reflexive)(x)), settings);
     });
     it('infinity', () => {
         function infinity(x, y) {
             return x === y && 1/x < 1/y 
         }
-        fc.assert(fc.property(fc.anything(), fc.anything(), (x, y) => !infinity(x, y)), settings);
+        fc.assert(fc.property(fc.anything(), fc.anything(), (x, y) => wrapIt(infinity)(x, y)), settings);
     });
     it('transitive', () => {
         function transitive(x,y,z) {
             return x && x == y && y == z && x != z;
         }
-        fc.assert(fc.property(fc.anything(), fc.anything(), fc.anything(), (x, y, z) => !transitive(x, y, z)), settings);
+        fc.assert(fc.property(fc.anything(), fc.anything(), fc.anything(), (x, y, z) => wrapIt(transitive)(x, y, z)), settings);
     });
     it('peano', () => {
         function peano(x) {
             return (x++ !== x) && (x++ === x);
         }
-        fc.assert(fc.property(fc.anything(), x => !peano(x)), settings);
+        fc.assert(fc.property(fc.anything(), x => wrapIt(peano)(x)), settings);
     });
 });
